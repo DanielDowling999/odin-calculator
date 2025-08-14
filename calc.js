64cd126a810cf099a0bdaaa67126c67e8a13b30e
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2025-08-14 14:49:20 +1200

    Added 'operate' command that will handle all inputs

--- calc.js
+++ calc.js
@@ -10,3 +10,24 @@ function multiply(x,y){
 function divide(x,y){
     return x/y;
 }
+
+let num1, num2, operation;
+
+function operate(x, y, sign){
+    switch(sign){
+        case "+":
+            return add(x,y);
+            break;
+        case "-":
+            return subtract(x,y);
+            break;
+        case "x":
+            return multiply(x,y);
+            break;
+        case "/":
+            return divide(x,y);
+            break;
+        default:
+            console.log("Error, unknown button pressed");
+    }
+}
